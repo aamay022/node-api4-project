e56c848c36ed50bd3b31a40489374947f7ff6573
Anthony <aamay022@fiu.edu>
2021-12-02 14:11:57 -0500

    2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 const server = express()
-
-const PORT =process.env.PORT || 8080
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/api/users', (req,res)=> {
     res.json([
@@ -12,6 +13,12 @@ server.get('/api/users', (req,res)=> {
         {id: 3, username: 'hey'},
     ])
 })
+
+server.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+const PORT =process.env.PORT || 8080
 
 server.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)
